68d2bbe13616bb4f07a526dcc99788f74cc7509f
joescaos <jxexcxo@gmail.com>
2023-06-04 21:21:00 -0500

    graph ds

--- src/graph/Main.java
+++ src/graph/Main.java
@@ -5,6 +5,12 @@ public class Main {
     public static void main(String[] args) {
         Graph myGraph = new Graph();
         myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        myGraph.addVertex("C");
+        myGraph.addEdge("A", "B");
+        myGraph.addEdge("A", "C");
+        myGraph.addEdge("B", "C");
+        myGraph.removeEdge("A", "B");
         myGraph.printGraph();
     }
 }
