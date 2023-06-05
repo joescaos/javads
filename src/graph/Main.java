370cc990e3950b637f154494b54109f2e5b02228
joescaos <jxexcxo@gmail.com>
2023-06-04 21:12:32 -0500

    graph ds

---
+++ src/graph/Main.java
@@ -0,0 +1,10 @@
+package graph;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Graph myGraph = new Graph();
+        myGraph.addVertex("A");
+        myGraph.printGraph();
+    }
+}
