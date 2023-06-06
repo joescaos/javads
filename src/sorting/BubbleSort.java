d479411cd0bc8647bad91610b6647ba87c65af7e
joescaos <jxexcxo@gmail.com>
2023-06-06 07:54:54 -0500

    bubble sort

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,31 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int [] arr = {4, 6, 2, 12, 34, 5, 9};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i <  n- 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1 ; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+}
