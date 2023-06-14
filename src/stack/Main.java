41bcd97736566fa4b60ac395c4dc611a05817f91
joescaos <jxexcxo@gmail.com>
2023-06-13 22:17:03 -0500

    stack algorithms

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -2,8 +2,21 @@ package stack;
 
 public class Main {
 
+    public static String reverseString(String s) {
+        StackGeneric<Character> stack = new StackGeneric<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            stack.push(s.charAt(i));
+        }
+        int size = stack.size();
+        for (int i = 0; i < size; i++) {
+            sb.append(stack.pop());
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
-        Stack stack = new Stack(5);
+        /*Stack stack = new Stack(5);
 
         stack.push(4);
         stack.push(6);
@@ -14,6 +27,19 @@ public class Main {
         System.out.println();
         System.out.println(stack.pop().value);
         System.out.println();
-        stack.printStack();
+        stack.printStack();*/
+
+        StackGeneric<Integer> stackGeneric = new StackGeneric<>();
+        stackGeneric.push(1);
+        stackGeneric.push(2);
+        stackGeneric.push(3);
+        stackGeneric.printStack();
+        System.out.println("element pop: " + stackGeneric.pop());
+        System.out.println();
+        stackGeneric.printStack();
+
+        String myString = "hello";
+        String reversedString = reverseString(myString);
+        System.out.println(reversedString);
     }
 }
