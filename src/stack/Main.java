942000b4ce61596bf20385bc720860e7369d87ab
joescaos <jxexcxo@gmail.com>
2023-06-14 21:57:42 -0500

    stack algorithms

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -15,33 +15,33 @@ public class Main {
         return sb.toString();
     }
 
-    public static boolean isBalancedParentheses(String parentheses) {
-        // Create an empty stack of characters
+    public static boolean isBalancedParentheses(String s) {
         StackGeneric<Character> stack = new StackGeneric<>();
-
-        // Iterate through each character in the input string
-        for (char p : parentheses.toCharArray()) {
-            // If the current character is an opening parenthesis, push it onto the stack
-            if (p == '(') {
-                stack.push(p);
-            }
-            // If the current character is a closing parenthesis, check if the stack is empty or
-            // if the top element of the stack is not an opening parenthesis. If either of these
-            // conditions is true, return false, because the parentheses are not balanced.
-            else if (p == ')') {
-                if (stack.isEmpty() || stack.pop() != '(') {
-                    return false;
-                }
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            } else if (stack.isEmpty() || stack.pop() != '(') {
+                return false;
             }
         }
 
-        // After iterating through all the characters in the input string, check if the stack is
-        // empty. If the stack is empty, return true, because all the opening parentheses have been
-        // matched with closing parentheses. Otherwise, return false, because there are still some
-        // opening parentheses left on the stack that have not been matched with closing parentheses.
         return stack.isEmpty();
     }
 
+    public static  void sortStack(StackGeneric<Integer> stackGeneric) {
+        StackGeneric<Integer> additionalStack = new StackGeneric<>();
+        while (!stackGeneric.isEmpty()) {
+            Integer temp = stackGeneric.pop();
+            while (!additionalStack.isEmpty() && (additionalStack.peek() > temp)) {
+                stackGeneric.push(additionalStack.pop());
+
+            }
+            additionalStack.push(temp);
+        }
+        while (!additionalStack.isEmpty()) {
+            stackGeneric.push(additionalStack.pop());
+        }
+    }
 
     public static void main(String[] args) {
         /*Stack stack = new Stack(5);
@@ -69,6 +69,42 @@ public class Main {
         /*String myString = "hello";
         String reversedString = reverseString(myString);
         System.out.println(reversedString);*/
-        System.out.println(isBalancedParentheses("())()"));
+        System.out.println(isBalancedParentheses("()()()"));
+
+        StackGeneric<Integer> stack = new StackGeneric<>();
+        stack.push(3);
+        stack.push(2);
+        stack.push(5);
+        stack.push(1);
+        stack.push(4);
+
+        System.out.println("Before sorting:");
+        stack.printStack();
+
+        sortStack(stack);
+
+        System.out.println("\nAfter sorting:");
+        stack.printStack();
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Before sorting:
+            4
+            1
+            5
+            2
+            3
+
+            After sorting:
+            1
+            2
+            3
+            4
+            5
+
+        */
+
     }
+
 }
