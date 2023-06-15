84defba99b3e044bba5381ad1606a1d1daefe22b
joescaos <jxexcxo@gmail.com>
2023-06-14 20:31:22 -0500

    stack algorithms

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -15,6 +15,34 @@ public class Main {
         return sb.toString();
     }
 
+    public static boolean isBalancedParentheses(String parentheses) {
+        // Create an empty stack of characters
+        StackGeneric<Character> stack = new StackGeneric<>();
+
+        // Iterate through each character in the input string
+        for (char p : parentheses.toCharArray()) {
+            // If the current character is an opening parenthesis, push it onto the stack
+            if (p == '(') {
+                stack.push(p);
+            }
+            // If the current character is a closing parenthesis, check if the stack is empty or
+            // if the top element of the stack is not an opening parenthesis. If either of these
+            // conditions is true, return false, because the parentheses are not balanced.
+            else if (p == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            }
+        }
+
+        // After iterating through all the characters in the input string, check if the stack is
+        // empty. If the stack is empty, return true, because all the opening parentheses have been
+        // matched with closing parentheses. Otherwise, return false, because there are still some
+        // opening parentheses left on the stack that have not been matched with closing parentheses.
+        return stack.isEmpty();
+    }
+
+
     public static void main(String[] args) {
         /*Stack stack = new Stack(5);
 
@@ -38,8 +66,9 @@ public class Main {
         System.out.println();
         stackGeneric.printStack();
 
-        String myString = "hello";
+        /*String myString = "hello";
         String reversedString = reverseString(myString);
-        System.out.println(reversedString);
+        System.out.println(reversedString);*/
+        System.out.println(isBalancedParentheses("())()"));
     }
 }
