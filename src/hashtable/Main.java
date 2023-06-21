f0a3d3acf886774776b636e811cc8bcd164b057b
joescaos <jxexcxo@gmail.com>
2023-06-20 20:27:53 -0500

    hashtable algorithms

--- src/hashtable/Main.java
+++ src/hashtable/Main.java
@@ -1,6 +1,10 @@
 package hashtable;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -17,6 +21,30 @@ public class Main {
             }
         }
         return false;
+    }
+
+    public static List<Integer> findDuplicates(int[] nums) {
+        Map<Integer, Integer> duplicatesMap = new HashMap<>();
+
+        for (int num : nums) {
+            duplicatesMap.put(num, duplicatesMap.getOrDefault(num, 0) + 1);
+        }
+
+
+        return duplicatesMap.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() > 1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+        /*List<Integer> values = new ArrayList<>();
+        for (int key : duplicatesMap.keySet()) {
+            if (duplicatesMap.get(key) > 1) {
+                values.add(key);
+            }
+        }
+
+        return values;*/
     }
 
     public static void main(String[] args) {
@@ -45,5 +73,10 @@ public class Main {
         int[] array2 = {2, 4, 5};
 
         System.out.println(itemInCommon(array1, array2));
+
+        System.out.println();
+        int[] nums = {1, 2, 3, 2, 1, 4, 5, 4};
+        List<Integer> duplicates = findDuplicates(nums);
+        System.out.println(duplicates);
     }
 }
